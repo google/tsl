17b1f33db4a5a68d8aad5b6e3df3dab27a2fb933
Michael Hudgins <michaelhudgins@google.com>
2023-02-13 16:13:40 -0500

    Update build.bat
    Is it just the tmp dirs?

--- .kokoro/windows/build.bat
+++ .kokoro/windows/build.bat
@@ -1,9 +1,4 @@
 
-SET BAZEL_SH=c:\tools\msys64\usr\bin\bash.exe
-SET PATH=%PATH%;C:\tools\msys64\usr\bin\
-C:\tools\msys64\usr\bin\pacman -S --noconfirm patch
-SET GCS_BUCKET_NAME=tensorflow-windows-cache
-SET GOOGLE_CLOUD_CREDENTIAL=%KOKORO_BAZEL_AUTH_CREDENTIAL:\=/%
 SET TMPDIR=T:/tmp
 SET TMP=%TMPDIR%
 SET TEMP=%TMPDIR%
