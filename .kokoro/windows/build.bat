1a311ab1cf2deae92fdb53ab974651862900f459
Michael Hudgins <michaelhudgins@google.com>
2023-02-13 15:41:49 -0500

    Update build.bat
    That worked, lets cut back some now

--- .kokoro/windows/build.bat
+++ .kokoro/windows/build.bat
@@ -8,31 +8,5 @@ SET TMPDIR=T:/tmp
 SET TMP=%TMPDIR%
 SET TEMP=%TMPDIR%
 
-
-@REM
-@REM Set Environment Variables
-@REM
-IF NOT DEFINED PYTHON_DIRECTORY (
-  SET PYTHON_DIRECTORY=Python39
-)
-SET PY_EXE=C:\%PYTHON_DIRECTORY%\python.exe
-SET PATH=%PATH%;C:\%PYTHON_DIRECTORY%
-
-@REM First, upgrade pypi wheels
-%PY_EXE% -m pip install --upgrade "setuptools" pip wheel
-
-@REM NOTE: Windows doesn't have any additional requirements from the common ones.
-%PY_EXE% -m pip install -r tensorflow/tools/ci_build/release/requirements_common.txt
-
-@REM
-@REM Setup Bazelisk
-@REM
-:: Download Bazelisk from GitHub and make sure its found in PATH.
-md C:\tools\bazel\
-wget -q https://github.com/bazelbuild/bazelisk/releases/download/v1.11.0/bazelisk-windows-amd64.exe -O C:/tools/bazel/bazel.exe
-SET PATH=C:\tools\bazel;%PATH%
-bazel version
-
-
 bash -l %0/../windows_build.sh %*
 exit /b %ERRORLEVEL%
