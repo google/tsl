openxla/xla

commit 9ae5ea6de92ef797f574305f0ef545b9572a7d76

PR #2842: According to GetNvccOptions of third_party/gpus/crosstool/windows/msvc_wrapper_for_nvcc.py.tpl, options pass directly to nvcc compiler driver need to be prefixed with -nvcc_options=, otherwise, they are actually passed to host compiler.
Merging this change closes #2842

PiperOrigin-RevId: 530422566

third_party/gpus/cuda/build_defs.bzl.tpl
@@ -69,7 +69,7 @@ def cuda_default_copts():
         if_nvcc = [
             "-Xcuda-fatbinary=--compress-all",
             # Ensure that NVCC matches clang's constexpr behavior.
-            "--expt-relaxed-constexpr"
+            "-nvcc_options=-expt-relaxed-constexpr"
         ]
     )
 

third_party/tsl/third_party/gpus/cuda/build_defs.bzl.tpl
@@ -69,7 +69,7 @@ def cuda_default_copts():
         if_nvcc = [
             "-Xcuda-fatbinary=--compress-all",
             # Ensure that NVCC matches clang's constexpr behavior.
-            "--expt-relaxed-constexpr"
+            "-nvcc_options=-expt-relaxed-constexpr"
         ]
     )
 

google/tsl

commit 8a5f5333f14c531cec54e62b02b23d841600a5ee

Fix compilation error in nvcc build.
https://github.com/openxla/xla/commit/9ae5ea6de92ef797f574305f0ef545b9572a7d76
introduced a nvcc compilation error.

```
nvcc fatal   : Unknown option '---expt-relaxed-constexpr'
```

Remove the extra dash.

PiperOrigin-RevId: 530499659

third_party/gpus/cuda/build_defs.bzl.tpl
@@ -69,7 +69,7 @@ def cuda_default_copts():
         if_nvcc = [
             "-Xcuda-fatbinary=--compress-all",
             # Ensure that NVCC matches clang's constexpr behavior.
-            "-nvcc_options=-expt-relaxed-constexpr"
+            "-nvcc_options=expt-relaxed-constexpr"
         ]
     )
 